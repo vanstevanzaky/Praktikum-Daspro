ef9ede804774199d86a9e408d969dc6f14e7e91a
vanstevanzaky <143470195+vanstevanzaky@users.noreply.github.com>
2023-10-15 15:30:01 +0700

    Praktik Daspro

---
+++ Minggu 7/DoWhileCuti29.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class DoWhileCuti29{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = sc.nextInt();
+        sc.nextLine();
+
+        do {
+            System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.nextLine();         
+        
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+
+                } else {
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+            }
+        } while (jatahCuti > 0);
+    }
+}
