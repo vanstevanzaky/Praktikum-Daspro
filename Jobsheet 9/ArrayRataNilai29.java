032e23ea862dd7700fed774b60a06663c7905016
vanstevanzaky <143470195+vanstevanzaky@users.noreply.github.com>
2023-10-28 22:37:01 +0700

    Praktik Daspro

--- Jobsheet 9/ArrayRataNilai29.java
+++ Jobsheet 9/ArrayRataNilai29.java
@@ -2,24 +2,31 @@ import java.util.Scanner;
 public class ArrayRataNilai29{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
-        int mahasiswaLulus = 0; 
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMahasiswa = sc.nextInt();
+        int[] nilaiMhs = new int[jumlahMahasiswa];
+        double totalLulus = 0;
+        double totalTidakLulus = 0;
+        int lulus = 0;
+        int tidakLulus = 0;
 
-        for (int i = 0; i < nilaiMhs.length; i++){
+
+        for (int i = 0; i < jumlahMahasiswa; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
             if (nilaiMhs[i] > 70) {
-                mahasiswaLulus++;
+                totalLulus += nilaiMhs[i];
+                lulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                tidakLulus++;
             }
         }
 
-        for (int i = 0; i < nilaiMhs.length; i++){
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
-        System.out.println("Jumlah mahasiswa yang lulus: " + mahasiswaLulus);
+        double rataLulus = lulus > 0 ? totalLulus / lulus : 0;
+        double rataTidakLulus = tidakLulus > 0 ? totalTidakLulus / tidakLulus : 0;
+
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
     }
 }
