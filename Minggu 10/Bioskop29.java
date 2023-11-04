5faa2607ad4cf394d072a036907b836af2745530
vanstevanzaky <143470195+vanstevanzaky@users.noreply.github.com>
2023-11-04 20:19:50 +0700

    Praktik Daspro

--- Minggu 10/Bioskop29.java
+++ Minggu 10/Bioskop29.java
@@ -22,5 +22,10 @@ public class Bioskop29{
         for (String[] barisPenonton : penonton) {
             System.out.println("Panjang baris: " + barisPenonton.length);
         }
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i = 0; i < penonton[2].length; i++){
+            System.out.println(penonton[2][i]);
+        }
+
     }
 }
