22aea864ce986ba62c5c934469650bc90bf148ae
vanstevanzaky <143470195+vanstevanzaky@users.noreply.github.com>
2023-11-05 21:09:31 +0700

    Praktik Daspro

--- Minggu 10/Number29.java
+++ Minggu 10/Number29.java
@@ -3,9 +3,6 @@ public class Number29{
     public static void main(String[] args){
         int barisSama = 5;
         int[][] myNumber = new int [3][barisSama];
-        myNumber[0] = new int[5];
-        myNumber[1] = new int[3];
-        myNumber[2] = new int[1];
 
         for (int i = 0; i < myNumber.length; i++){
             System.out.println(Arrays.toString(myNumber[i]));
