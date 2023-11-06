bf932dccf2cfd5a719cdace08770ff10bedb1ca3
vanstevanzaky <143470195+vanstevanzaky@users.noreply.github.com>
2023-11-06 20:20:25 +0700

    Praktik Daspro

--- Week 11/Star29.java
+++ Week 11/Star29.java
@@ -4,7 +4,7 @@ public class Star29{
         Scanner  sc = new Scanner(System.in);
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
-        for (int i = 1; i <=N; i++){
+        for (int i = 0; i <=N; i++){
             System.out.print("*");
         }
         
