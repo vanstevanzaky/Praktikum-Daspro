20604b6662c184b80505777029ea71d7aa30ecc2
vanstevanzaky <143470195+vanstevanzaky@users.noreply.github.com>
2023-11-11 13:15:24 +0700

    Praktik Daspro

--- Week 11/NestedLoop_2341720101.java
+++ Week 11/NestedLoop_2341720101.java
@@ -3,6 +3,7 @@ public class NestedLoop_2341720101{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         double temps[][] = new double[5][7];
+        
 
         for(int i = 0; i < temps.length; i++){
             System.out.println("Kota ke-"+i);
@@ -14,10 +15,13 @@ public class NestedLoop_2341720101{
         }
         for (int i = 0; i < temps.length; i++){
             System.out.print("Kota ke-" + (i + 1) + ":");
-            for (int j = 0; j < temps[0].length; j++){
-                System.out.print(temps[i][j] + " ");
+            double total = 0;
+            for (double temp : temps[i]){
+                System.out.print(temp+" ");
+                total += temp;
             }
-            System.out.println();
+            double rata = total / temps[i].length;
+            System.out.println("Rata-rata:"+ rata);
         }
     }
 }
