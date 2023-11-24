edd5ac4a5afe64ae29a98cfb7cf02211ae79ba26
vanstevanzaky <143470195+vanstevanzaky@users.noreply.github.com>
2023-11-24 20:45:38 +0700

    Praktik  Daspro

--- Week 13/TerimaKasih_29.java
+++ Week 13/TerimaKasih_29.java
@@ -4,8 +4,14 @@ public class TerimaKasih_29{
         "You inspired in me a lover for learning and made me feel like I could ask you anything. ");
 
     }
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
+    }
     public static void main(String[] args){
         UcapanTerimaKasih();
+        String ucapan = "Terima kasih Pak.. Bu.. Semoga Sehat Sealalu";
+        UcapanTambahan(ucapan);
+
 
     }
         
